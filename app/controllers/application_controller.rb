aab81db4e8697f629bfbdf8e83d17d40b372ac4c
Raphael D. Pinheiro <raphaklaus@gmail.com>
2014-09-27 20:44:14 -0700

    Web Api - Launch Register

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::API
+ after_filter :cors_set_access_control_headers
+
+
+def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin']      = '*'
+    headers['Access-Control-Allow-Methods']     = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age']           = '1728000'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+  end	
+end
