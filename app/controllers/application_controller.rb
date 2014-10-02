925f03f13a4002a6920c14607508df9eba1dada2
Raphael Pinheiro <raphaklaus@gmail.com>
2014-10-02 20:26:04 -0300

    Final adjusts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
 
 def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin']      = 'http://www.garageplay.net/'
+    headers['Access-Control-Allow-Origin']      = 'http://www.garageplay.net'
     headers['Access-Control-Allow-Methods']     = 'POST, OPTIONS'
     headers['Access-Control-Max-Age']           = '1728000'
     headers['Access-Control-Allow-Credentials'] = 'true'
